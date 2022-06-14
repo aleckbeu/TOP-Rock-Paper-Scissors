53469c5b26834fa6237cda21b35813ec1d820446
Alejandro <alejandroariasag@gmail.com>
2022-06-14 16:14:17 +0200

    Changed the naming of he functions to VERBS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 let startButton = document.getElementById('startButton');
 let computerScore = document.getElementById('computerScore')
 let playerScore = document.getElementById('playerScore')
-let numberOfRounds = document.getElementById('numberOfRounds')
+let numberOftotalRounds = document.getElementById('numberOfRounds')
 let charmander = document.getElementById('charmander')
 let squirtle = document.getElementById('squirtle')
 let bulbasaur = document.getElementById('bulbasaur')
@@ -13,48 +13,93 @@ let computerSelection;
 
 let playerScoreTotal;
 let computerScoreTotal;
-let rounds = 0;
+
+let totalRounds = 0;
+let currentRound = 0;
+
+console.log(computerScore.innerText);
+console.log(playerScore.innerText);
+
+const MESSAGES = [
+    'Let´s the game begin! Choose your Pókemon, the computer will choose randomly.',
+    'Second Round!',
+    'Third Round!',
+    'Forth Round',
+    'Final ROUND!!!'
+]
 
 startButton.addEventListener('click', function() {
     startGame();
 });
 
+function checkRound(totalRounds, currentRound) {
+    if (currentRound === totalRounds) {
+        console.log('Total Rounds: ' + totalRounds + 'Current Round:' + currentRound);
+        return true;
 
-console.log(startButton);
-console.log(computerScore);
-console.log(playerScore);
-console.log(numberOfRounds);
-console.log(charmander);
-console.log(squirtle);
-console.log(bulbasaur);
-console.log(board);
-console.log(message);
+    } else {
+        console.log(totalRounds, currentRound);
+        return false;
 
-
+    }
+}
 
 function startGame() {
     board.classList.toggle('visually-hidden');
-    message.innerText = "Let´s the game begin! Choose your Pókemon, the computer will choose randomly.";
+    message.innerText = MESSAGES[0];
+
 
 
     squirtle.addEventListener('click', function() {
-        playerSelection = 'squirtle';
-        humanPlay(playerSelection);
+
+        if (checkRound(totalRounds, currentRound) === true) {
+            currentRound++;
+            console.log('Add +1 to Current Round');
+            numberOftotalRounds.innerText = totalRounds;
+            console.log('Total Rounds: ' + totalRounds + 'Current Round:' + currentRound);
+
+            playerSelection = 'squirtle';
+            playHuman(playerSelection);
+        } else {
+            squirtle.classList.add('pe-none');
+        }
+
     });
 
     bulbasaur.addEventListener('click', function() {
-        playerSelection = 'bulbasaur';
-        humanPlay(playerSelection);
+
+        if (checkRound(totalRounds, currentRound) === true) {
+            currentRound++;
+            console.log('Add +1 to Current Round');
+            numberOftotalRounds.innerText = totalRounds;
+            console.log('Total Rounds: ' + totalRounds + 'Current Round:' + currentRound);
+
+            playerSelection = 'bulbasaur';
+            playHuman(playerSelection);
+        } else {
+            bulbasaur.classList.add('pe-none');
+        }
     });
 
     charmander.addEventListener('click', function() {
-        playerSelection = 'charmander';
-        humanPlay(playerSelection);
+
+        if (checkRound(totalRounds, currentRound) === true) {
+            currentRound++;
+            console.log('Add +1 to Current Round');
+            numberOftotalRounds.innerText = totalRounds;
+            console.log('Total Rounds: ' + totalRounds + 'Current Round:' + currentRound);
+
+            playerSelection = 'charmander';
+            playHuman(playerSelection);
+        } else {
+            charmander.classList.add('pe-none');
+        }
     });
 
 }
 
-function computerPlay(playerSelection) {
+function playComputer(playerSelection) {
+
     const listOfPokemons = ['charmander', 'squirtle', 'bulbasaur'];
 
     let randomPokemon = Math.floor(Math.random() * listOfPokemons.length);
@@ -69,12 +114,12 @@ function computerPlay(playerSelection) {
     message.innerText = "The computer has chosen " + computerSelection;
 
 
-    setTimeout(battleCheck, 3000, computerSelection, playerSelection);
+    setTimeout(checkBattle, 3000, computerSelection, playerSelection);
 
 
 }
 
-function humanPlay(playerSelection) {
+function playHuman(playerSelection) {
 
     console.log('Human Selection: ' + playerSelection);
     let element = document.getElementById(playerSelection);
@@ -82,63 +127,105 @@ function humanPlay(playerSelection) {
 
     message.innerText = "You´ve chosen " + playerSelection;
 
-    playRound(playerSelection);
+    setTimeout(playComputer, 3000, playerSelection);
 
 }
 
-function playRound(playerSelection) {
-    rounds++;
-    numberOfRounds.innerText = rounds;
-
-    setTimeout(computerPlay, 3000, playerSelection);
-
-}
-
-function battleCheck(computerSelection, playerSelection) {
+function checkBattle(computerSelection, playerSelection) {
     if (computerSelection === playerSelection) {
         message.innerText = "Empate";
+        console.log('EMPATE');
+        totalRounds = totalRounds + 1;
+        console.log('Add +1 to Total Rounds');
+
+
     }
 
     if (computerSelection === 'charmander' && playerSelection === 'squirtle') {
         message.innerText = "You´ve win! Water beats Fire!";
-        playerScoreTotal++;
-        playerScore.innerText++;
+        console.log("You´ve win! Water beats Fire!");
+        playerScoreTotal = playerScoreTotal + 1;
+        playerScore.innerText = playerScoreTotal;
+        totalRounds = totalRounds + 1;
+        console.log('Add +1 to Total Rounds');
+
+
+
+
     }
 
     if (computerSelection === 'charmander' && playerSelection === 'bulbasaur') {
         message.innerText = "You´ve lost! Fire beats Grass!";
-        computerScoreTotal++;
-        computerScoreTotal.innerText++;
+        console.log("You´ve lost! Fire beats Grass!");
+        computerScoreTotal = computerScoreTotal + 1;
+        computerScore.innerText = computerScoreTotal;
+        totalRounds = totalRounds + 1;
+        console.log('Add +1 to Total Rounds');
+
+
+
+
     }
     if (computerSelection === 'squirtle' && playerSelection === 'charmander') {
         message.innerText = "You´ve win! Water beats Fire!";
-        playerScoreTotal++;
-        playerScore.innerText++;
+        console.log("You´ve win! Water beats Fire!");
+        playerScoreTotal = playerScoreTotal + 1;
+        playerScore.innerText = playerScoreTotal;
+        totalRounds = totalRounds + 1;
+        console.log('Add +1 to Total Rounds');
+
+
+
+
     }
 
     if (computerSelection === 'squirtle' && playerSelection === 'bulbasaur') {
         message.innerText = "You´ve lost! Grass beats Water!";
-        computerScoreTotal++;
-        computerScoreTotal.innerText++;
+        console.log("You´ve lost! Grass beats Water!");
+        computerScoreTotal = computerScoreTotal + 1;
+        computerScore.innerText = computerScoreTotal;
+        totalRounds = totalRounds + 1;
+        console.log('Add +1 to Total Rounds');
+
+
+
+
     }
     if (computerSelection === 'bulbasaur' && playerSelection === 'charmander') {
         message.innerText = "You´ve win! Fire beats Grass!";
-        playerScoreTotal++;
-        playerScore.innerText++;
+        console.log("You´ve win! Fire beats Grass!");
+        playerScoreTotal = playerScoreTotal + 1;
+        playerScore.innerText = playerScoreTotal;
+        totalRounds = totalRounds + 1;
+        console.log('Add +1 to Total Rounds');
+
+
+
     }
 
     if (computerSelection === 'bulbasaur' && playerSelection === 'squirtle') {
         message.innerText = "You´ve lost! Water beats Grass!";
-        computerScoreTotal++;
-        computerScoreTotal.innerText++;
+        console.log("You´ve lost! Water beats Grass!");
+        computerScoreTotal = computerScoreTotal + 1;
+        computerScore.innerText = computerScoreTotal;
+        totalRounds = totalRounds + 1;
+        console.log('Add +1 to Total Rounds');
+
+
+
     }
 
     setTimeout(resetChooses, 3000, computerSelection, playerSelection);
 }
 
+
+
+
+
+// TRABAJAR EN HACER MÁXIMO 5 RONDAS
+
 function resetChooses(computerSelection, playerSelection) {
-    console.log(computerSelection, playerSelection);
-    message.innerText = "Second Round!! Choose your Pokémon!";
+    message.innerText = MESSAGES[totalRounds];
 
     let foo = document.getElementById(playerSelection);
     foo.classList.remove('border', 'border-5', 'border-primary');
@@ -148,4 +235,6 @@ function resetChooses(computerSelection, playerSelection) {
 
     playerSelection = '';
     computerSelection = '';
+    console.log('Total Rounds: ' + totalRounds + 'Current Round:' + currentRound);
+    console.log('RESET');
 }
